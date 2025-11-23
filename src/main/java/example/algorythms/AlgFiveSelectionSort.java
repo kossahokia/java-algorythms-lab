cd58e6d968042c7218927fbdb7e37605a674f36f
black20lion <Kos.Sahokia@yandex.ru>
2025-11-23 17:02:38 +0300

    feat: algorythm #5 selection sort was added

---
+++ src/main/java/example/algorythms/AlgFiveSelectionSort.java
@@ -0,0 +1,36 @@
+package example.algorythms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlgFiveSelectionSort {
+    public static void main(String[] args) {
+        List<Integer> testArray = new ArrayList<>(List.of(5, 4, 42, 1, 20, 96, 43, 43, 93, 85, 11, 113, 13));
+        System.out.println(testArray);
+        System.out.println(selectionSort(testArray));
+    }
+
+    public static int findSmallest(List<Integer> array) {
+        Integer smallest = array.getFirst();
+        int smallest_index = 0;
+
+        for (int i = 1; i < array.size(); i++) {
+            if (array.get(i) < smallest) {
+                smallest = array.get(i);
+                smallest_index = i;
+            }
+        }
+        return smallest_index;
+    }
+
+    public static List<Integer> selectionSort(List<Integer> array) {
+        List<Integer> newArray = new ArrayList<>();
+        int size = array.size();
+        for (int i = 0; i < size; i++) {
+            int smallestIndex = findSmallest(array);
+            newArray.add(array.get(smallestIndex));
+            array.remove(smallestIndex);
+        }
+        return newArray;
+    }
+}
